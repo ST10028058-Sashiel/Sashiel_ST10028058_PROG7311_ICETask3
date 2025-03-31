893beec818727a53052dcdbbcdb520830fd2320e
ST10028058-Sashiel <128804235+ST10028058-Sashiel@users.noreply.github.com>
2025-03-31 18:39:46 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Sashiel_ST10028058_PROG7311_ICETask3.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
